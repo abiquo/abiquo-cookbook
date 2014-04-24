e5a34b46702aeb30faa6da87c15bfe5c2b036325
Ignasi Barrera <nacx@apache.org>
2014-04-24 09:18:32 +0200

    Allow to configure an internal api location

--- attributes/default.rb
+++ attributes/default.rb
@@ -24,6 +24,10 @@ default['abiquo']['installdb'] = true
 default['abiquo']['license'] = nil
 # default['abiquo']['license'] = "license-code"
 
+# Determines if the "api location" property will use the internal
+# or the external HTTP port (the Tomcat or the Apache one)
+default['abiquo']['api']['internal'] = false
+
 # Use this property to configure the yum repository with the nightly packages
 default['abiquo']['nightly-repo'] = nil
 #default['abiquo']['nightly-repo'] = "http://10.60.20.42/master/rpm"
--- recipes/configure.rb
+++ recipes/configure.rb
@@ -15,7 +15,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-api_port = node['abiquo']['http-protocol'] == 'https'? 443 : node['apache']['listen_ports'].first
+if node['abiquo']['api']['internal']
+    api_port = node['abiquo']['tomcat-http-port']
+elsif node['abiquo']['http-protocol'] == 'https'
+    api_port = 443
+else
+    api_port = node['apache']['listen_ports'].first
+end
 api_location = "#{node['abiquo']['http-protocol']}://#{node['ipaddress']}:#{api_port}/api"
 
 unless node['abiquo']['nfs']['location'].nil?
