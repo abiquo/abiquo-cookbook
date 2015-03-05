f77d7e006c3dfc31a97a3cd3ed3f107dccdd26b8
Ignasi Barrera <nacx@apache.org>
2015-03-05 12:02:27 +0100

    Bump version to 0.3.5

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ignasi.barrera@abiquo.com'
 license          'Apache 2.0'
 description      'Installs and configures an Abiquo platform'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.4'
+version          '0.3.5'
 
 recipe 'abiquo', 'Installs and configures an Abiquo platform'
 recipe 'abiquo::repository', 'Configures the Abiquo yum repositories'
