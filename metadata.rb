9433d9cd71f896c7ad0007bc19f6fc62c8f12154
Marc Cirauqui <marc.cirauqui@abiquo.com>
2018-09-04 16:30:27 +0200

    Chef version 12

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/abiquo/abiquo-cookbook'
 issues_url       'https://github.com/abiquo/abiquo-cookbook/issues'
 version          '0.14.0'
-chef_version     '~> 12.16'
+chef_version     '~> 12'
 
 recipe 'abiquo', 'Installs and configures an Abiquo platform'
 recipe 'abiquo::repository', 'Configures the Abiquo yum repositories'
